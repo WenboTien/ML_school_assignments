7e11b61234b0c63d3409737087d683a650bddac4
Wenbo <wenbo.tian@asu.edu>
2017-01-31 23:21:32 -0700

    new hw

---
+++ project1/tiny1/sample_submission.py
@@ -0,0 +1,51 @@
+# sample_submission.py
+import numpy as np
+
+
+class regressor(object):
+    """
+    This is a sample class for miniproject 1.
+    Args:
+        data: Is a tuple, ``(x,y)``
+              ``x`` is a two or one dimensional ndarray ordered such that axis 0 is independent
+              data and data is spread along axis 1. If the array had only one dimension, it implies
+              that data is 1D.
+              ``y`` is a 1D ndarray it will be of the same length as axis 0 or x.
+
+    """
+
+    def __init__(self, data):
+        self.x, self.y = data
+        # Here is where your training and all the other magic should happen.
+        # Once trained you should have these parameters with ready.
+        self.w = np.dot(np.dot(np.linalg.inv(np.dot(self.x.T, self.x)), self.x.T), self.y)
+        self.b = self.y - np.dot(self.x, self.w)
+
+    def get_params(self):
+        """
+        Method that should return the model parameters.
+        Returns:
+            tuple of numpy.ndarray: (w, b).
+        Notes:
+            This code will return a random numpy array for demonstration purposes.
+        """
+        return (self.w, self.b)
+
+    def get_predictions(self, x):
+        """
+        Method should return the outputs given unseen data
+        Args:
+            x: array similar to ``x`` in ``data``. Might be of different size.
+        Returns:
+            numpy.ndarray: ``y`` which is a 1D array of predictions of th
+            e same length as axis 0 of
+                            ``x``
+        Notes:
+            Temporarily returns random numpy array for demonstration purposes.
+        """
+        # Here is where you write a code to evaluate the data and produce predictions.
+        return np.dot(x, self.w) + self.b
+
+
+if __name__ == '__main__':
+    pass
